5d35df4acadb24dce137ada6f3ce4f9af9b71a17
Muhammad Naufal Hafizh <71245137+NaufalHafizh15@users.noreply.github.com>
2020-09-17 15:01:51 +0700

    Update HelloSociety.java

--- Praktikum 2/HelloSociety.java
+++ Praktikum 2/HelloSociety.java
@@ -3,7 +3,7 @@ public class HelloSociety {
     public static void main (String[] args){
         System.out.println("Nama    : Muhammad Naufal Hafizh"); 
         System.out.println("NIM	    : 6706204003");
-        System.out.println("Kelas 	: D3IF 44-03");
+        System.out.println("Kelas   : D3IF 44-03");
 
         
     }
